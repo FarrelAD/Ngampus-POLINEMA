4ca43f375c1719e685ba732380e8a5a31e42c19c
Farrel Augusta Dinata <fadfarrel@gmail.com>
2023-10-25 16:11:58 +0700

    ADD: file LinearSearch12.java at Praktikum 9/Tugas

--- DASPRO/Praktikum/Praktikum 9/Tugas/LinearSearch12.java
+++ DASPRO/Praktikum/Praktikum 9/Tugas/LinearSearch12.java
@@ -1,17 +1,42 @@
+import java.util.Scanner;
+
 public class LinearSearch12 {
     public static void main(String[] args) {
-        
-        // Deklarasi variabel
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        Scanner scan12 = new Scanner(System.in);
 
+        // Deklarasi variabel
+        int[] arrayInt;
+        int inputArray;
+        int inputKey;
+        int hasil = -1;
+
+        // Input nilai ke arrayInt
+        System.out.print("Masukkan jumlah elemen array: ");
+        inputArray = scan12.nextInt();
+        arrayInt = new int[inputArray];
+
+        // Input nilai pada arrayInt sejumlah banyak elemen yang telah diinput sebelumnya
         for (int i = 0; i < arrayInt.length; i++) {
-            if (arrayInt[i] == key) {
+            System.out.print("Masukkan elemen array ke-" + (i + 1) + ": ");
+            arrayInt[i] = scan12.nextInt();
+        }
+
+        // Input nilai key yang ingin dicari
+        System.out.print("Masukkan key yang ingin anda cari: ");
+        inputKey = scan12.nextInt();
+
+        // Proses pencarian indeks nilai yang ingin dicari
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == inputKey) {
                 hasil = i;
-                break;
+                System.out.println("Key yang anda cari ada di posisi indeks ke-" + hasil);
+                break; // Hentikan loop setelah kunci ditemukan
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+ hasil);
+
+        // Cek apakah kunci ditemukan
+        if (hasil == -1) {
+            System.out.println("Key tidak ditemukan");
+        }
     }
 }
