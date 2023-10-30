11e14a31a6c463748e4448630a7e58a3202f5f05
Farrel Augusta Dinata <fadfarrel@gmail.com>
2023-10-30 12:12:40 +0700

    UPDATE: penambahan fungsi for-each-loop

--- DASPRO/Praktikum/Praktikum 10/Tugas/Bioskop12.java
+++ DASPRO/Praktikum/Praktikum 10/Tugas/Bioskop12.java
@@ -28,11 +28,19 @@ public class Bioskop12 {
         System.out.println(penonton[3].length);
 
         System.out.println("====================================================================");
-        
+
         // Menampilkan panjang setiap baris pada array
         System.out.println(penonton.length);
         for (int i = 0; i < penonton.length; i++) {
             System.out.println("Panjang baris ke-"+ (i+1) + ": " + penonton[i].length);
+        }
+
+        System.out.println("====================================================================");
+
+        // Menampilkan panjang setiap baris pada array menggunakan foreach loop
+        System.out.println(penonton.length);
+        for (String[] barisPentonton : penonton) {
+            System.out.println("Panjang baris: " + barisPentonton.length);
         }
     }
 }
