0522ff3cb032a4c6dba2adc747f19f6b3c9261ad
Farrel Augusta Dinata <fadfarrel@gmail.com>
2023-11-06 13:02:17 +0700

    ADD: file Square12 - Praktikum 11

---
+++ DASPRO/Praktikum/Praktikum 11/Square12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Square12 {
+    public static void main(String[] args) {
+        Scanner sc12 = new Scanner (System.in);
+        System.out.print("Masukkan nilai N = ");
+        int N = sc12.nextInt();
+        for (int iOuter = 1; iOuter <= N; iOuter++) {
+            for (int i = 0; i <= N; i++) {
+                System.out.print("*");
+            }
+            // System.out.println();
+        }
+        
+    }
+}
