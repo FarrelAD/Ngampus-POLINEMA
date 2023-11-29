d39d17315b0921229ec948b3c0c004a192a59c48
Farrel Augusta Dinata <fadfarrel@gmail.com>
2023-11-29 07:27:43 +0700

    feat: tambah cara baru memanggil fungsi

--- Semester 1/Praktikum DASPRO/12-Praktikum 13/Tugas/Kubus_12.java
+++ Semester 1/Praktikum DASPRO/12-Praktikum 13/Tugas/Kubus_12.java
@@ -1,22 +1,38 @@
 import java.util.Scanner;
 public class Kubus_12 {
     static Scanner scan12 = new Scanner (System.in);
-    static int sisiKubus, luasSisi;
+    static int  luasSisi;
     public static void main(String[] args) {
         System.out.print("Inputkan nilai sisi panjang kubus: ");
-        sisiKubus = scan12.nextInt();
-        VolumeKubus(sisiKubus);
-        LuasPermukaanKubus();
+        int sisiKubus = scan12.nextInt();
+        // Langsung memanggil fungsi
+        // VolumeKubus(sisiKubus); 
+        // LuasPermukaanKubus();
+        // Mencetak sekaligus memanggil fungsi VolumeKubus
+        System.out.printf("Volume kubusnya adalah: %d\n", VolumeKubus(sisiKubus)); 
+        System.out.printf("Luas permukaan kubusnya adalah: %d", LuasPermukaanKubus());
     }
+
+    // public static void VolumeKubus(int sisiKubus) {
+    //     luasSisi  = sisiKubus * sisiKubus;
+    //     int volumeKubus = luasSisi * sisiKubus;
+    //     System.out.print("Volume kubus anda: " + volumeKubus);
+    //     System.out.println("");
+    // }
+
     public static int VolumeKubus(int sisiKubus) {
-        luasSisi  = sisiKubus * sisiKubus;
+        luasSisi = sisiKubus * sisiKubus;
         int volumeKubus = luasSisi * sisiKubus;
-        System.out.print("Volume kubus anda: " + volumeKubus);
-        System.out.println("");
-        return luasSisi;
+        return volumeKubus;
     }
-    public static void LuasPermukaanKubus() {
+
+    // public static void LuasPermukaanKubus() {
+    //     int luasPermukaanKubus = luasSisi * 6;
+    //     System.out.print("Luas permukaan kubus anda: " + luasPermukaanKubus);
+    // }
+
+    public static int LuasPermukaanKubus() {
         int luasPermukaanKubus = luasSisi * 6;
-        System.out.print("Luas permukaan kubus anda: " + luasPermukaanKubus);
+        return luasPermukaanKubus;
     }
 }
