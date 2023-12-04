0621ba4191353ef4e3cc960faa3551bc16bd9f20
Farrel Augusta Dinata <fadfarrel@gmail.com>
2023-12-04 10:31:44 +0700

    feat: upload file Mahasiswa2_12.java

---
+++ Semester 1/Praktikum DASPRO/12-Praktikum 13/Tugas/Mahasiswa2_12.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+public class Mahasiswa2_12 {
+    // Deklarasi array 2 dimensi untuk menyimpan nilai mahasiswa
+    static int[][] dataMahasiswa;
+    static String[] namaMahasiswa;
+    static int mingguTertinggi = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String enter;
+
+        // Input jumlah mahasiswa
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = scanner.nextInt();
+        scanner.nextLine(); // Mengonsumsi newline
+
+        // Input jumlah tugas
+        System.out.print("Masukkan jumlah tugas: ");
+        int jumlahTugas = scanner.nextInt();
+        scanner.nextLine(); // Mengonsumsi newline
+
+        // Menginisialisasi array dengan ukuran yang telah ditentukan
+        dataMahasiswa = new int[jumlahMahasiswa][jumlahTugas];
+        namaMahasiswa = new String[jumlahMahasiswa];
+
+        // Input data nilai mahasiswa
+        inputDataNilai(scanner);
+        System.out.println("Enter untuk melanjutkan --> ");
+        enter = scanner.nextLine();
+
+        // Menampilkan seluruh nilai mahasiswa
+        tampilkandataMahasiswa();
+        System.out.println("Enter untuk melanjutkan --> ");
+        enter = scanner.nextLine();
+
+        // Mencari minggu dengan nilai tertinggi
+        mingguTertinggi = cariMingguTertinggi() + 1;
+        System.out.println("Enter untuk melanjutkan --> ");
+        enter = scanner.nextLine();
+
+        // Cari dan tampil mahasiswa dengan nilai tertinggi
+        cariMahasiswaNilaiTertinggi();
+        System.out.println("Enter untuk melanjutkan --> ");
+        enter = scanner.nextLine();
+
+        scanner.close();
+    }
+
+    // Fungsi untuk menginputkan data nilai mahasiswa
+    public static void inputDataNilai(Scanner scanner) {
+        System.out.println("--!!INPUT NILAI MAHASISWA!!--");
+        for (int i = 0; i < namaMahasiswa.length; i++) {
+            System.out.print("Masukkan nama mahasiswa ke-" + (i + 1) + ": ");
+            namaMahasiswa[i] = scanner.nextLine();
+
+            for (int j = 0; j < dataMahasiswa[1].length; j++) {
+                System.out.print("Masukkan nilai tugas minggu ke-" + (j + 1) + " untuk " + namaMahasiswa[i] + ": ");
+                dataMahasiswa[i][j] = scanner.nextInt();
+            }
+            scanner.nextLine();
+        }
+    }
+
+    // Fungsi untuk menampilkan seluruh nilai mahasiswa
+    public static void tampilkandataMahasiswa() {
+        System.out.println("--!!TAMPILKAN NILAI MAHASISWA!!--");
+        for (int i = 0; i < namaMahasiswa.length; i++) {
+            System.out.printf("Nama mahasiswa: %s%n", namaMahasiswa[i]);
+            for (int j = 0; j < dataMahasiswa[1].length; j++) {
+                System.out.printf("Nilai tugas ke-%d : %s %n", (j+1), dataMahasiswa[i][j]);
+            }
+            System.out.println("-----------------------------------");
+            System.out.println("");
+        }
+    }
+
+    // Mencari hari dengan nilai tertinggi
+    public static int cariMingguTertinggi() {
+        System.out.println("--!!MINGGU NILAI TERTINGGI!!--");
+        int indexMingguTertinggi = 0;
+        int nilaiTertinggi = 0;
+        for (int i = 0; i < namaMahasiswa.length; i++ ) {
+            for (int j = 0; j < dataMahasiswa[1].length; j++) {
+                if (dataMahasiswa[i][j] > nilaiTertinggi) {
+                    nilaiTertinggi = dataMahasiswa[i][j];
+                    indexMingguTertinggi = j;
+                }
+            }
+        }
+        System.out.printf("Minggu dengan nilai tertingginya adalah --> %d%n", (indexMingguTertinggi + 1));
+        return indexMingguTertinggi;
+    }
+
+    // Mencari nilai tertinggi
+    public static void cariMahasiswaNilaiTertinggi() {
+        System.out.println("--!!MAHASISWA NILAI TERTINGGI!!--");
+        int nilaiTertinggi = 0;
+        int indexMahasiswa = 0;
+        String mahasiswaNilaiTertinggi = null;
+        for (int i = 0; i < namaMahasiswa.length; i++) {
+            for (int j = 0; j < dataMahasiswa[1].length; j++) {
+                if (dataMahasiswa[i][j] > nilaiTertinggi) {
+                    nilaiTertinggi = dataMahasiswa[i][j];
+                    indexMahasiswa = i;
+                }
+            }
+        }
+
+        for (int i = 0; i < namaMahasiswa.length; i++ ) {
+            if (i == indexMahasiswa) {
+                mahasiswaNilaiTertinggi = namaMahasiswa[i];
+            }
+        }
+
+        System.out.printf("Nilai tertinggi dari keseluruhan mahasiswa \t--> %d%n", nilaiTertinggi);
+        System.out.printf("Mahasiswa dengan nilai tertinggi \t\t--> %s%n", mahasiswaNilaiTertinggi);
+        System.out.printf("Didapatkan pada minggu ke \t\t\t--> %d%n", mingguTertinggi);
+    }
+}
