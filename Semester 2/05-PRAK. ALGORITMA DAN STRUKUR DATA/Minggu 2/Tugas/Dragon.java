78cd5270583ed1e761a3c378d5c31e9d64d01efd
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-02-23 14:29:07 +0700

    add: printPosition and main method

--- Semester 2/05-PRAK. ALGORITMA DAN STRUKUR DATA/Minggu 2/Tugas/Dragon.java
+++ Semester 2/05-PRAK. ALGORITMA DAN STRUKUR DATA/Minggu 2/Tugas/Dragon.java
@@ -2,9 +2,12 @@
 // JOBSHEET 2 
 // 21 Februari 2024
 
+import java.util.Scanner;
+
 public class Dragon {
     // Below is the attribute of the Dragon class
     int x, y, width, height;
+    boolean isContinue = false;
 
 
     // Below is method for Dragon class
@@ -41,6 +44,61 @@ public class Dragon {
     }
 
     void detectCollision(int x, int y) {
-        System.out.println("Game Over");
+        isContinue = true;
+        System.out.println("------------------------------------");
+        System.out.println("GAME OVER!");
+        System.out.println("------------------------------------");
+    }
+
+    void printPosition() {
+        System.out.printf("POSITION x: %d - y: %d\n", x, y);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Dragon myDragon = new Dragon();
+
+        System.out.print(
+            "SET UP YOUR PLAYING AREA\n" +
+            "- HEIGHT : "
+        );
+        myDragon.height = scan.nextInt();
+        System.out.print("- WIDTH : ");
+        myDragon.width = scan.nextInt();
+
+        System.out.println("------------------------------------");
+
+        do {
+            System.out.print(
+                "PILIH ARAH!\n" +
+                "1. KE ATAS\n" +
+                "2. KE BAWAH\n" +
+                "3. KE KANAN\n" +
+                "4. KE KIRI\n" +
+                "input> "
+            );
+            int direction = scan.nextInt();
+
+            switch (direction) {
+                case 1:
+                    myDragon.moveUp();
+                    break;
+                case 2:
+                    myDragon.moveDown();
+                    break;
+                case 3:
+                    myDragon.moveRight();
+                    break;
+                case 4:
+                    myDragon.moveLeft();
+                    break;
+                default:
+                    break;
+            }
+            // Print the dragon position
+            myDragon.printPosition();
+            System.out.println("------------------------------------");
+        } while (!myDragon.isContinue);
     }
 }
