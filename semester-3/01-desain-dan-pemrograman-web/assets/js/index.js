7b0a456ef40037d207de00640ee77eada8d003c6
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-09-11 22:49:39 +0700

    add web project data 2nd week - web basic

--- semester-3/01-desain-dan-pemrograman-web/assets/js/index.js
+++ semester-3/01-desain-dan-pemrograman-web/assets/js/index.js
@@ -1,14 +1,17 @@
-const weekOptions = document.querySelectorAll('.week-option');
+const weekOptions = document.querySelectorAll('.week-option')
+const weekProjectsData = document.querySelectorAll('.week-project-data')
 
 let currentWeek = 0;
 weekOptions.forEach((week, index) => {
     week.addEventListener('click', function() {
+        weekProjectsData[currentWeek].style.display = 'none'
         weekOptions[currentWeek].style.backgroundColor = 'white'
         weekOptions[currentWeek].style.color = 'black'
         weekOptions[currentWeek].style.fontWeight = 'normal'
+        currentWeek = index
+        weekProjectsData[currentWeek].style.display = 'grid'
         this.style.backgroundColor = 'black'
         this.style.color = 'white'
         this.style.fontWeight = 'bold'
-        currentWeek = index
     })
 });
