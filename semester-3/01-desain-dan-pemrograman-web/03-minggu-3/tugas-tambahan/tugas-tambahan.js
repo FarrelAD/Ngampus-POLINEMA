a143cc900e1c34352d24221e94df1f6bc699ae53
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-09-14 20:07:32 +0700

    update javascript and more managable html elements

--- semester-3/01-desain-dan-pemrograman-web/03-minggu-3/tugas-tambahan/tugas-tambahan.js
+++ semester-3/01-desain-dan-pemrograman-web/03-minggu-3/tugas-tambahan/tugas-tambahan.js
@@ -1,3 +1,6 @@
+//////////////////////////////////////
+// CSS
+//////////////////////////////////////
 const head = document.getElementsByTagName('head')[0]
 const style = document.createElement('style')
 style.textContent = `
@@ -8,15 +11,23 @@ style.textContent = `
         min-height: 100vh;
         min-width: 100vw;
         max-width: 100vw;
-        max-height: 100vh;
         display: flex;
         justify-content: center;
         align-items: center;
     }
 
-    form {
+    #main {
+        margin: 2rem 0;
+    }
+
+    #form-header,
+    #form-body {
         background-color: white;
-        padding: 2rem;;
+        padding: 2rem;
+    }
+
+    #form-header {
+        margin-bottom: 1rem;
     }
 
     .form-field {
@@ -25,105 +36,177 @@ style.textContent = `
         margin: 1rem 0;
     }
 
+    #form-name,
+    #form-address,
+    #form-date-of-birth {
+        width: 40%;
+    }
+
+    textarea {
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+
     .form-label-input {
         width: 50%;
     }
 `
 head.appendChild(style)
 
-const main = document.getElementById('main')
-main.innerHTML = `
-    <form action="/register" method="post">
-        <div class="form-field">
-            <label for="form-name" class="form-label-input">Nama</label>
-            <input type="text" name="name" id="form-name" class="form-input" placeholder="Contoh: Andi Rekayasa">
-        </div>
-        <div class="form-field">
-            <label for="form-address" class="form-label-input">Alamat</label>
-            <textarea name="address" id="form-address" class="form-input" rows="5" cols="30" placeholder="Contoh: Jalan Mawar No. 23 Desa Mekar Kecamatan Jaya Kabupaten Sukses"></textarea>
-        </div>
-        <div class="form-field">
-            <label for="form-date-of-birth" class="form-label-input">Tanggal lahir</label>
-            <input type="date" name="date-of-birth" id="form-date-of-birth" class="form-input">
-        </div>
-        <div class="form-field">
-            <div class="form-label-input">
-                Jenis kelamin
-            </div>
-            <div>
-                <div>
-                    <input type="radio" name="gender" id="form-gender-male" class="form-input" value="male">
-                    Laki-laki
-                </div>
-                <div>
-                    <input type="radio" name="gender" id="form-gender-female" class="form-input"value="female">
-                    Perempuan
-                </div>
-                <input type="hidden" name="gender" id="form-gender-null" value="">
-            </div>
-        </div>
-        <div class="form-field">
-            <div class="form-label-input">Hobi</div>
-            <div>
-                <div>
-                    <input type="checkbox" name="hobbies" value="swimming">
-                    Berenang
-                </div>
-                <div>
-                    <input type="checkbox" name="hobbies" value="reading">
-                    Membaca
-                </div>
-                <div>
-                    <input type="checkbox" name="hobbies" values="drawing">
-                    Menggambar
-                </div>
-                <div>
-                    <input type="checkbox" name="hobbies" value="others">
-                    Lain-lain
-                </div>
-                <input type="hidden" name="hobbies" id="form-hobbies-null" value="">
-            </div>
-        </div>
-        <div class="form-field">
-            <div class="form-label-input">Pekerjaan</div>
-            <div>
-                <div>
-                    <input type="checkbox" name="jobs" value="soldier">
-                    Tentara
-                </div>
-                <div>
-                    <input type="checkbox" name="jobs" value="programmer">
-                    Programmer
-                </div>
-                <div>
-                    <input type="checkbox" name="jobs" value="designer">
-                    Designer
-                </div>
-                <div>
-                    <input type="checkbox" name="jobs" value="3d-illustrator">
-                    3D illustrator
-                </div>
-                <div>
-                    <input type="checkbox" name="jobs" value="others">
-                    Lain-lain
-                </div>
-                <input type="hidden" name="jobs" id="form-jobs-null" value="">
-            </div>
-        </div>
 
-        <div id="aggrement-input">
-            <input type="checkbox">
-            Saya sudah menyetujui seluruh persyaratan yang ada dan bersedia data saya digunakan di sistem kami
-        </div>
 
-        <button type="submit">Kirim</button>
-    </form>
+//////////////////////////////////////
+// HTML
+//////////////////////////////////////
+const formHeader = document.createElement('div')
+formHeader.id = 'form-header'
+formHeader.innerHTML = `
+    <h1>Formulir Pendaftaran Penjaga Galaksi Bima Sakti</h1>
 `
 
+const nameFormField = document.createElement('div')
+nameFormField.id = 'name-form-field'
+nameFormField.className = 'form-field'
+nameFormField.innerHTML = `
+    <label for="form-name" class="form-label-input">Nama</label>
+    <input type="text" name="name" id="form-name" class="form-input" placeholder="Contoh: Andi Rekayasa">
+`
+
+const addressFormField = document.createElement('div')
+addressFormField.id = 'address-form-field'
+addressFormField.className = 'form-field'
+addressFormField.innerHTML = `
+    <label for="form-address" class="form-label-input">Alamat</label>
+    <textarea name="address" id="form-address" class="form-input" rows="5" cols="30" placeholder="Contoh: Jalan Mawar No. 23 Desa Mekar Kecamatan Jaya Kabupaten Sukses"></textarea>
+`
+
+const dateOfBirthFormField = document.createElement('div')
+dateOfBirthFormField.id = 'date-of-birth-form-field'
+dateOfBirthFormField.className = 'form-field'
+dateOfBirthFormField.innerHTML = `
+    <label for="form-date-of-birth" class="form-label-input">Tanggal lahir</label>
+    <input type="date" name="date-of-birth" id="form-date-of-birth" class="form-input">
+
+`
+
+const genderFormField = document.createElement('div')
+genderFormField.id = 'gender-form-field'
+genderFormField.className = 'form-field'
+genderFormField.innerHTML = `
+    <div class="form-label-input">
+        Jenis kelamin
+    </div>
+    <div>
+        <div>
+            <input type="radio" name="gender" id="form-gender-male" class="form-input form-gender" value="male">
+            Laki-laki
+        </div>
+        <div>
+            <input type="radio" name="gender" id="form-gender-female" class="form-input form-gender" value="female">
+            Perempuan
+        </div>
+        <input type="hidden" name="gender" id="form-gender-null" class="form-input" value="">
+    </div>
+`
+
+const hobbiesFormField = document.createElement('div')
+hobbiesFormField.id = 'hobbies-form-field'
+hobbiesFormField.className = 'form-field'
+hobbiesFormField.innerHTML = `
+    <div class="form-label-input">Hobi</div>
+    <div>
+        <div>
+            <input type="checkbox" name="hobbies" class="form-input form-hobbies" value="swimming">
+            Berenang
+        </div>
+        <div>
+            <input type="checkbox" name="hobbies" class="form-input form-hobbies" value="reading">
+            Membaca
+        </div>
+        <div>
+            <input type="checkbox" name="hobbies" class="form-input form-hobbies" values="drawing">
+            Menggambar
+        </div>
+        <div>
+            <input type="checkbox" name="hobbies" class="form-input form-hobbies" value="others">
+            Lain-lain
+        </div>
+        <input type="hidden" name="hobbies" id="form-hobbies-null" class="form-input" value="">
+    </div>
+`
+
+const jobsFormField = document.createElement('div')
+jobsFormField.id = 'jobs-form-field'
+jobsFormField.className = 'form-field'
+jobsFormField.innerHTML = `
+    <div class="form-label-input">Pekerjaan</div>
+    <div>
+        <div>
+            <input type="checkbox" name="jobs" class="form-input form-jobs" value="soldier">
+            Tentara
+        </div>
+        <div>
+            <input type="checkbox" name="jobs" class="form-input form-jobs" value="programmer">
+            Programmer
+        </div>
+        <div>
+            <input type="checkbox" name="jobs" class="form-input form-jobs" value="designer">
+            Designer
+        </div>
+        <div>
+            <input type="checkbox" name="jobs" class="form-input form-jobs" value="3d-illustrator">
+            3D illustrator
+        </div>
+        <div>
+            <input type="checkbox" name="jobs" class="form-input form-jobs" value="others">
+            Lain-lain
+        </div>
+        <input type="hidden" name="jobs" id="form-jobs-null" class="form-input" value="">
+    </div>
+`
+
+const aggrementFormField = document.createElement('div')
+aggrementFormField.id = 'aggrement-input'
+aggrementFormField.innerHTML = `
+    <input type="checkbox">
+    Saya telah berkomitmen untuk menjadi bagian dari pasukan galaksi Bima Sakti
+`
+
+const buttonFormSubmit = document.createElement('button')
+buttonFormSubmit.type = 'submit'
+buttonFormSubmit.textContent = 'Kirim'
+
+const formBody = document.createElement('form')
+formBody.id = 'form-body'
+formBody.action = '/register'
+formBody.method = 'post'
+formBody.appendChild(nameFormField)
+formBody.appendChild(addressFormField)
+formBody.appendChild(dateOfBirthFormField)
+formBody.appendChild(genderFormField)
+formBody.appendChild(hobbiesFormField)
+formBody.appendChild(jobsFormField)
+formBody.appendChild(aggrementFormField)
+formBody.appendChild(buttonFormSubmit)
+
+const main = document.getElementById('main')
+main.appendChild(formHeader)
+main.appendChild(formBody)
+
+
+
+//////////////////////////////////////
+// JAVASCRIPT
+//////////////////////////////////////
 const formInput = document.querySelectorAll('.form-input')
+const nameInputField = document.getElementById('form-name')
+const addressInputField = document.getElementById('address-form-field')
+const dateOfBirthInputField = document.getElementById('form-date-of-birth')
+const genderInputField = document.getElementsByClassName('form-gender')
+const hobbiesInputField = document.getElementsByClassName('form-hobbies')
+const jobsInputField = document.getElementsByClassName('form-jobs')
 
 formInput.forEach(el => {
-    el.addEventListener('input', function() {
+    el.addEventListener('input', function () {
         if (this.value == '') {
             this.style.borderRadius = '.25rem'
             this.style.border = '2px solid red'
@@ -138,7 +221,7 @@ formInput.forEach(el => {
 let isFormDataComplete = true
 
 const form = document.getElementsByTagName('form')[0]
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
     isFormDataComplete = true
     event.preventDefault()
 
@@ -147,12 +230,12 @@ form.addEventListener('submit', function(event) {
     let genderInput = 0
     let hobbiesInput = 0
     let jobsInput = 0
-    
+
     formData.forEach((value, key) => {
         switch (key) {
             case 'name':
             case 'address':
-            case 'date-of-birth':            
+            case 'date-of-birth':
                 if (value === '') {
                     isFormDataComplete = false
                 }
@@ -173,7 +256,30 @@ form.addEventListener('submit', function(event) {
         isFormDataComplete = false
     }
 
-    console.log('nilai [2]: ' + isFormDataComplete)
+    if (genderInput == 1) {
+        Array.from(genderInputField).forEach(el => {
+            el.style.borderRadius = '50%'
+            el.style.border = '2px solid red'
+            el.style.outline = '5px solid rgba(255, 0, 0, 0.2)'
+        })
+    }
+
+    if (hobbiesInput == 1) {
+        Array.from(hobbiesInputField).forEach(el => {
+            el.style.borderRadius = '.25rem'
+            el.style.border = '2px solid red'
+            el.style.outline = '5px solid rgba(255, 0, 0, 0.2)'
+        })
+    }
+
+    if (jobsInput == 1) {
+        Array.from(jobsInputField).forEach(el => {
+            el.style.borderRadius = '.25rem'
+            el.style.border = '2px solid red'
+            el.style.outline = '5px solid rgba(255, 0, 0, 0.2)'
+        })
+    }
+
     if (!isFormDataComplete) {
         alert('Formulir tidak bisa dikirim! Pastikan seluruh data sudah terisi terlebih dahulu!')
     } else {
